53ba500a7e7b90ddc87d938b5c8c30ecd8b4c40f
Ayush Srivastava <ayushsri9666@gmail.com>
2024-02-28 23:25:48 +0530

    uppercase lowercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,23 +2,37 @@ import React, {useState} from 'react';
 
 
 export default function TextForm(props) {
+    const [text, setText] = useState('Enter Text Here !');
+    const handleOnChange = (event) =>{
+        // console.log("On Change");
+        setText(event.target.value);
+    }
     const handleUpClick = () =>{
         // console.log("UpperCase was clicked "+text);
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleOnChange = (event) =>{
-        // console.log("On Change");
-        setText(event.target.value);
+    const handleLowClick = () =>{
+        let newText = text.toLowerCase();
+        setText(newText);
     }
-    const [text, setText] = useState('Enter Text Here !');
   return (
-    <div>
+    <>
+    <div className='container'>
         <div className="mb-3">
-            <h1>{props.heading}</h1>
-            <textarea className="form-control" value= {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+            <h3>{props.heading}</h3>
+            <textarea className="form-control" value= {text} onChange = {handleOnChange} id="myBox" rows="5"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick} onChange={handleOnChange}>Convert to UpperCase</button>
+        <button className="btn btn-primary ms-2" onClick={handleLowClick} onChange={handleOnChange}>Convert to LowerCase</button>
     </div>
+    <div className="container mt-3">
+        <h3>Your Text Summary</h3>
+        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
