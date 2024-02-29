ac31f6799b77d9bc3203ea5fc0e39d0ca055bda1
Ayush Srivastava <ayushsri9666@gmail.com>
2024-02-29 13:31:39 +0530

    dark

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
 function App() {
   return (
     <>
+<div className="container-fluid">
 <Navbar title="TextUtils" aboutText="About Us"/>
 <div className="container my-3">
 <TextForm heading = "Enter The Text To Analyze : "/>
 </div>
- 
+</div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,4 @@
 import React, {useState} from 'react';
-
-
 export default function TextForm(props) {
     const [text, setText] = useState('');
     const handleOnChange = (event) =>{
@@ -23,17 +21,41 @@ export default function TextForm(props) {
         let newText = text.toString();
         setText(newText);
     }
+  // Dark Mode
+    const [mode, setMode] = useState({
+        color : 'black',
+        backgroundColor : 'white'
+      })
+      const [btnText, setBtnText] = useState('Enable Dark Mode')
+      const toggleStyle = () =>{
+          if(mode.color === 'white'){
+            setMode({
+              color : 'black',
+              backgroundColor : 'white'
+            });
+            setBtnText("Enable Dark Mode");
+          }
+            else{
+              setMode({
+                color : 'white',
+                backgroundColor : '#000000de'
+              });
+              setBtnText("Diable Dark Mode")
+            }
+              
+          }
   return (
     <>
-    <div className='container'>
-        <div className="mb-3">
+    <div className='container-fluid p-4' style={mode}>
+        <div className="pb-3">
+        <button onClick={toggleStyle} style={mode} className="btn btn-dark mb-3">{btnText}</button>
             <h3>{props.heading}</h3>
-            <textarea className="form-control" value= {text} onChange = {handleOnChange} id="myBox" rows="5"></textarea>
+            <textarea className="form-control" style={mode} value= {text} onChange = {handleOnChange} id="myBox" rows="5"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick} onChange={handleOnChange}>Convert to UpperCase</button>
-        <button className="btn btn-primary ms-2" onClick={handleLowClick} onChange={handleOnChange}>Convert to LowerCase</button>
-        <button className="btn btn-primary ms-2" onClick={handleLocLowClick} onChange={handleOnChange}>Convert to LowerCase</button>
-        <button className="btn btn-danger ms-2" onClick={handleClearClick} onChange={handleOnChange}>Clear</button>
+        <button className="btn btn-primary" style={mode} onClick={handleUpClick} onChange={handleOnChange}>Convert to UpperCase</button>
+        <button className="btn btn-primary ms-2" style={mode} onClick={handleLowClick} onChange={handleOnChange}>Convert to LowerCase</button>
+        <button className="btn btn-primary ms-2" style={mode} onClick={handleLocLowClick} onChange={handleOnChange}>Convert to LowerCase</button>
+        <button className="btn btn-danger ms-2" style={mode} onClick={handleClearClick} onChange={handleOnChange}>Clear</button>
     </div>
     <div className="container mt-3">
         <h3>Your Text Summary</h3>
